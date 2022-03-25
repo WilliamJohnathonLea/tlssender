164cac708b7d0578999716f09282bf641a58bbcf
William Lea <william.lea@imgarena.com>
2022-03-25 12:19:34 +0000

    add support for non TLS traffic

--- internal/senders.go
+++ internal/senders.go
@@ -3,17 +3,18 @@ package internal
 import (
 	"crypto/tls"
 	"io/ioutil"
+	"net"
 	"os"
 )
 
 // The maximum size of a TCP frame in bytes
 const maxFrameSize = 256
 
-// SendFile sends the contents of a file to a TCP connection.
+// SecureSendFile sends the contents of a file to a TCP connection.
 // filepath is the path to the data to be sent.
 // conn is the connection where the data will be sent.
 // if ack is == true then we will make sure the data was received with an 'OK'
-func SendFile(filepath string, conn *tls.Conn, ack bool) error {
+func SecureSendFile(filepath string, conn *tls.Conn, ack bool) error {
 	bytes, err := os.ReadFile(filepath)
 	if err != nil {
 		return err
@@ -30,11 +31,51 @@ func SendFile(filepath string, conn *tls.Conn, ack bool) error {
 	return err
 }
 
+// SendFile sends the contents of a file to a TCP connection.
+// filepath is the path to the data to be sent.
+// conn is the connection where the data will be sent.
+// if ack is == true then we will make sure the data was received with an 'OK'
+func SendFile(filepath string, conn net.Conn, ack bool) error {
+	bytes, err := os.ReadFile(filepath)
+	if err != nil {
+		return err
+	}
+	_, err = conn.Write(bytesToTCPMessage(bytes))
+	if ack {
+		respBytes := make([]byte, maxFrameSize)
+		nRead, err := conn.Read(respBytes)
+		if err != nil {
+			return err
+		}
+		return handleAck(respBytes[:nRead])
+	}
+	return err
+}
+
+// SecureSendDir sends the contents of files in a directory to a TCP connection.
+// dirpath is the path to the data directory.
+// conn is the connection where the data will be sent.
+// If ack is == true then we will make sure the data was received with an 'OK'
+func SecureSendDir(dirpath string, conn *tls.Conn, ack bool) error {
+	files, err := ioutil.ReadDir(dirpath)
+	if err != nil {
+		return err
+	}
+	for _, f := range files {
+		filepath := dirpath + "/" + f.Name()
+		err = SecureSendFile(filepath, conn, ack)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 // SendDir sends the contents of files in a directory to a TCP connection.
 // dirpath is the path to the data directory.
 // conn is the connection where the data will be sent.
 // If ack is == true then we will make sure the data was received with an 'OK'
-func SendDir(dirpath string, conn *tls.Conn, ack bool) error {
+func SendDir(dirpath string, conn net.Conn, ack bool) error {
 	files, err := ioutil.ReadDir(dirpath)
 	if err != nil {
 		return err
--- main.go
+++ main.go
@@ -10,41 +10,40 @@ import (
 
 const (
 	fileCmdStr = "file"
-	dirCmdStr = "dir"
+	dirCmdStr  = "dir"
 )
 
 func main() {
+	var useSecureConnection bool = false
+	var ack bool = false
+
 	fileCmd := flag.NewFlagSet(fileCmdStr, flag.ExitOnError)
-	fileAck := fileCmd.Bool("ack", false, "Get acknowlegment of sent packet")
+	fileCmd.BoolVar(&ack, "ack", false, "Get acknowlegment of sent packet")
+	fileCmd.BoolVar(&useSecureConnection, "secure", false, "Use TLS secure connection")
 	dirCmd := flag.NewFlagSet(dirCmdStr, flag.ExitOnError)
-	dirAck := dirCmd.Bool("ack", false, "Get acknowlegment for each sent packet")
+	dirCmd.BoolVar(&ack, "ack", false, "Get acknowlegment for each sent packet")
+	dirCmd.BoolVar(&useSecureConnection, "secure", false, "Use TLS secure connection")
 
 	switch os.Args[1] {
 	case fileCmdStr:
 		fileCmd.Parse(os.Args[2:])
 		hostAndPort := fileCmd.Arg(0)
 		filepath := fileCmd.Arg(1)
-		conn, err := internal.ConnectTCP(hostAndPort)
-		if err != nil {
-			log.Fatalf("failed to connect: %v", err.Error())
-		}
-		defer conn.Close()
-		err = internal.SendFile(filepath, conn, *fileAck)
-		if err != nil {
-			log.Fatalf("failed while sending file: %v", err)
+
+		if useSecureConnection {
+			internal.SecureHandleFile(hostAndPort, filepath, ack)
+		} else {
+			internal.HandleFile(hostAndPort, filepath, ack)
 		}
 	case dirCmdStr:
 		dirCmd.Parse(os.Args[2:])
 		hostAndPort := fileCmd.Arg(0)
 		dir := fileCmd.Arg(1)
-		conn, err := internal.ConnectTCP(hostAndPort)
-		if err != nil {
-			log.Fatalf("failed to connect: %v", err.Error())
-		}
-		defer conn.Close()
-		err = internal.SendDir(dir, conn, *dirAck)
-		if err != nil {
-			log.Fatalf("failed while sending files: %v", err)
+
+		if useSecureConnection {
+			internal.SecureHandleDir(hostAndPort, dir, ack)
+		} else {
+			internal.HandleDir(hostAndPort, dir, ack)
 		}
 	default:
 		log.Fatalf("expected subcommand '%s' or '%s'", fileCmdStr, dirCmdStr)
